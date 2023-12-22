f97b5521512ad6b5be098267436f3c118b16496c
Amanda Woods <acwoods314@gmail.com>
2023-12-22 08:43:59 -0800

    Carousel now fully styled, needs animation

--- src/Carousel.js
+++ src/Carousel.js
@@ -18,13 +18,14 @@ const Carousel = ({ images }) => {
 
     return (
         <div className='carousel'>
+            <div className='carousel-images'>
             <img key={currentIndex-1} src={images[currentIndex-1]} />
             <img key={currentIndex} src={images[currentIndex]} />
             <img key={currentIndex+1} src={images[currentIndex+1]} />
             <div className='slide-direction'>
                 <div className='left' onClick={handlePrevious}>
                     <svg xmlns='http://www.w3.org/2000/svg' height='20' width='20' viewBox='0 96 960 960'>
-                        <path d='M400 976 0 5761400-400 56 57-343 343 343 343-56 57Z' />
+                        <path d='M400 976 0 576l400-400 56 57-343 343 343 343-56 57Z' />
                     </svg>
                 </div>
                 <div className='right' onClick={handleNext}>
@@ -32,6 +33,7 @@ const Carousel = ({ images }) => {
                         <path d='m304 974-56-57 343-343-343-343 56-57 400 400-400 400Z' />
                     </svg>
                 </div>
+            </div>
             </div>
             <div className='indicator'>
                 {images.map((_, index) => (
