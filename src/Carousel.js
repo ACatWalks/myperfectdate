04c9427d60df568fbf2acb7e7b342875e0249958
Amanda Woods <acwoods314@gmail.com>
2023-12-27 09:35:57 -0800

    Final touches on carousel, edit page improved, now displays labels for preferred race

--- src/Carousel.js
+++ src/Carousel.js
@@ -5,11 +5,11 @@ const Carousel = ({ images }) => {
     const [currentIndex, setCurrentIndex] = useState(1)
 
     const handleNext = () => {
-        setCurrentIndex((prevIndex) => prevIndex + 2 === images.length ? 1 : prevIndex + 1)
+        setCurrentIndex((prevIndex) => prevIndex + 1 === images.length ? 1 : prevIndex + 1)
     }
 
     const handlePrevious = () => {
-        setCurrentIndex((prevIndex) => prevIndex - 2 < 0 ? images.length - 2 : prevIndex - 2)
+        setCurrentIndex((prevIndex) => prevIndex - 1 < 0 ? images.length - 1 : prevIndex - 1)
     }
 
     const handleDotClick = (index) => {
--- src/Edit.js
+++ src/Edit.js
@@ -19,6 +19,18 @@ function Edit() {
         id: sessionStorage.getItem('id')
     })
 
+    const [preferredRace, setPreferredRace] = useState(
+        new Array(7).fill(false)
+    )
+
+    const handleChange = (position) => {
+        const updatedPreferredRace = preferredRace.map((item, index) => 
+            index === position ? !item : item
+        )
+
+        setPreferredRace(updatedPreferredRace)
+    }
+
     async function handleSubmit(e){
         e.preventDefault()
         await fetch(`https://my-perfect-date.herokuapp.com/users/${user.email}`, {
@@ -34,7 +46,7 @@ function Edit() {
         sessionStorage.setItem('profilepic', user.profilepic)
         sessionStorage.setItem('user_race', user.userRace)
         sessionStorage.setItem('user_sex', user.userSex)
-        sessionStorage.setItem('preferred_race', user.preferredRace)
+        sessionStorage.setItem('preferred_race', preferredRace)
         sessionStorage.setItem('preferred_sex', user.preferredSex)
         sessionStorage.setItem('id', user.id)
         navigate(`/${userId}`)
@@ -85,13 +97,20 @@ function Edit() {
                 </div>
                 <div className='form-div'>
                     <h5>Which race(s), if any, do you prefer?</h5>
-                    <input type='checkbox' id='asian' name='preferred_race' value='Asian' onClick={e => user.preferredRace.push('Asian')} />
-                    <input type='checkbox' id='white' name='preferred_race' value='White' onClick={e => user.preferredRace.push('White')} />
-                    <input type='checkbox' id='hispanic' name='preferred_race' value='Hispanic' onClick={e => user.preferredRace.push('Hispanic')} />
-                    <input type='checkbox' id='native american' name='preferred_race' value='Native American' onClick={e => user.preferredRace.push('Native American')} />
-                    <input type='checkbox' id='black' name='preferred_race' value='Black' onClick={e => user.preferredRace.push('Black')} />
-                    <input type='checkbox' id='mixed race/other' name='preferred_race' onClick={e => user.preferredRace.push('Mixed Race/Other')} />
-                    <input type='checkbox' id='no preference' name='preferred_race' value='No preference' onClick={e => user.preferredRace.push('No preference')} />
+                    <input type='checkbox' id='asian' name='preferred_race' value='Asian' checked={preferredRace[0]} onChange={() => handleChange(0)} />
+                    <label htmlFor='Asian'>Asian</label>
+                    <input type='checkbox' id='white' name='preferred_race' value='White' checked={preferredRace[1]} onChange={() => handleChange(1)} />
+                    <label htmlFor='White'>White</label>
+                    <input type='checkbox' id='hispanic' name='preferred_race' value='Hispanic' checked={preferredRace[2]} onChange={() => handleChange(2)} />
+                    <label htmlFor='Hispanic'>Hispanic</label>
+                    <input type='checkbox' id='native_american' name='preferred_race' value='Native American' checked={preferredRace[3]} onChange={() => handleChange(3)} />
+                    <label htmlFor='Native American'>Native American</label>
+                    <input type='checkbox' id='black' name='preferred_race' value='Black' checked={preferredRace[4]} onChange={() => handleChange(4)} />
+                    <label htmlFor='Black'>Black</label>
+                    <input type='checkbox' id='mixed_race/other' name='preferred_race' value='Mixed Race/Other' checked={preferredRace[5]} onChange={() => handleChange(5)} />
+                    <label htmlFor='Mixed Race/Other'>Mixed Race/Other</label>
+                    <input type='checkbox' id='no_race_preference' name='preferred_race' value='No preference' checked={preferredRace[6]} onChange={() => handleChange(6)} />
+                    <label htmlFor='No preference'>No preference</label>
                 </div>
                 <div className='form-div'>
                     <h5>Are you seeking a:</h5>
@@ -99,7 +118,7 @@ function Edit() {
                     <label htmlFor='man'>Man</label>
                     <input type='radio' id='woman' name='preferred_sex' value='Woman' onClick={e => setUser({...user, preferredSex: 'Woman'})} />
                     <label htmlFor='woman'>Woman</label>
-                    <input type='radio' id='no preference' name='preferred_sex' value='No preference' onClick={e => setUser({...user, preferredSex: 'No preference'})} />
+                    <input type='radio' id='no_sex_preference' name='preferred_sex' value='No preference' onClick={e => setUser({...user, preferredSex: 'No preference'})} />
                     <label htmlFor='no preference'>No preference</label>
                 </div>
                 <div className='form-div'>
